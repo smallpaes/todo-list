4218cbd71530040d4bac88ff6f883665c88cba63
Mike <mike.huang.mikank@gmail.com>
2019-07-17 17:49:02 +0800

    Add home and user routers

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express()
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const userRoutes = require('./routes/user')
 
 // Include models
 const db = require('./models')
@@ -23,33 +24,8 @@ app.use(methodOverride('_method'))
 app.get('/', (req, res) => {
   res.send('hello world')
 })
-// 認證系統的路由
-// 登入頁面
-app.get('/users/login', (req, res) => {
-  res.render('login')
-})
-// 登入檢查
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
-// 註冊頁面
-app.get('/users/register', (req, res) => {
-  res.render('register')
-})
-// 註冊檢查
-app.post('/users/register', (req, res) => {
-  User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  })
-    .then(user => res.redirect('/'))
 
-})
-// 登出
-app.get('/users/logout', (req, res) => {
-  res.send('logout')
-})
+app.use('/users', userRoutes)
 
 // 設定 express port 3000
 app.listen(port, () => {
---
+++ routes/user.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const router = express.Router()
+
+// Include user model
+const db = require('../models')
+const User = db.User
+
+// 認證系統的路由
+// 登入頁面
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+// 登入檢查
+router.post('/login', (req, res) => {
+  res.send('login')
+})
+// 註冊頁面
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+// 註冊檢查
+router.post('/register', (req, res) => {
+  const { name, email, password, password2 } = req.body
+  User.findOne({ where: { email: email } })
+    .then(user => {
+      if (user) {
+        console.log('User already exists')
+        res.render('register', { name, email, password, password2 })
+      } else {
+        // create new user
+        const newUser = new User({
+          name,
+          email,
+          password
+        })
+        newUser
+          .save()
+          .then(user => {
+            res.redirect('/')
+          })
+          .catch(err => console.log(err))
+      }
+    })
+
+})
+// 登出
+router.get('/logout', (req, res) => {
+  res.send('logout')
+})
+
+module.exports = router
