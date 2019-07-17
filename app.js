c2096b9b97b4166b28c64690d9c9074ca6437947
Mike <mike.huang.mikank@gmail.com>
2019-07-17 11:25:09 +0800

    Initialize project, add routes and view for register and login page

--- app.js
+++ app.js
@@ -15,9 +15,29 @@ app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
-// 設定路由
 app.get('/', (req, res) => {
   res.send('hello world')
+})
+// 認證系統的路由
+// 登入頁面
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+// 登入檢查
+app.post('/users/login', (req, res) => {
+  res.send('login')
+})
+// 註冊頁面
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+// 註冊檢查
+app.post('/users/register', (req, res) => {
+  res.send('register')
+})
+// 登出
+app.get('/users/logout', (req, res) => {
+  res.send('logout')
 })
 
 // 設定 express port 3000
