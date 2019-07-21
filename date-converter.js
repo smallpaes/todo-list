00a1732641ceaadff91e1e9a8d91633ef141df59
Mike <mike.huang.mikank@gmail.com>
2019-07-21 12:46:15 +0800

    Fix date converter bug

--- date-converter.js
+++ date-converter.js
@@ -3,7 +3,7 @@ module.exports = {
     const formatedDate = new Date(originalDate)
     let month = formatedDate.getMonth() + 1
     month = month.toString().padStart(2, '0')
-    const date = formatedDate.getDate()
+    const date = formatedDate.getDate().toString().padStart(2, '0')
     const year = formatedDate.getFullYear()
     return `${year}-${month}-${date}`
   }
